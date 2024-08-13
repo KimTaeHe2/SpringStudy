b72c44e6a6c60a8405831cb60c5e4e25a86972e0
10-5\mit <xogml0423@nate.com>
2024-08-13 13:07:24 +0900

    V1.0.1 (JDBC 연결 테스트 성공)

---
+++ src/main/resources/root.sql
@@ -0,0 +1,7 @@
+create user book_ex identified by book_ex default tablespace users temporary tablespace temp;
+
+grant connect, dba to book_ex;
+
+select dbms_xdb.gethttpport() from dual; -- 8080 포트 사용 중임을 확인한다.
+
+exec dbms_xdb.sethttpport(9090); -- 이클립스에서 지원되지 않은 명령어 -> sqlplus에서 함.
